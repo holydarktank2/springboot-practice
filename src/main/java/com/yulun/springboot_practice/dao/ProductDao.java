d42511449970a8f395c6a7dbea9976d28e1dad73
holydarktank2 <614allenylchen@gmail.com>
2025-01-17 14:44:31 +0800

    新增 查詢商品列表功能

--- src/main/java/com/yulun/springboot_practice/dao/ProductDao.java
+++ src/main/java/com/yulun/springboot_practice/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.yulun.springboot_practice.dao;
 import com.yulun.springboot_practice.dto.ProductRequest;
 import com.yulun.springboot_practice.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- src/main/java/com/yulun/springboot_practice/service/ProductService.java
+++ src/main/java/com/yulun/springboot_practice/service/ProductService.java
@@ -3,7 +3,10 @@ package com.yulun.springboot_practice.service;
 import com.yulun.springboot_practice.dto.ProductRequest;
 import com.yulun.springboot_practice.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
