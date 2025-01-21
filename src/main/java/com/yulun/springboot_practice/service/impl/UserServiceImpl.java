da9a84549f4b508fb9d058a72d80039e006e9d89
holydarktank2 <614allenylchen@gmail.com>
2025-01-21 11:41:39 +0800

    新增 使用 MD5 加密密碼

--- src/main/java/com/yulun/springboot_practice/service/impl/UserServiceImpl.java
+++ src/main/java/com/yulun/springboot_practice/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -33,6 +34,9 @@ public class UserServiceImpl implements UserService {
             logger.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        //使用 MD5 雜湊涵式加密密碼
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
 
         //創建帳號
         return userDao.createUser(userRegisterRequest);
@@ -46,7 +50,10 @@ public class UserServiceImpl implements UserService {
             logger.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException((HttpStatus.BAD_REQUEST));
         }
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+
+        //使用 MD5 生成密碼雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+        if(user.getPassword().equals(hashedPassword)){
             return user;
         }else {
             logger.warn("email {} 密碼不正確", userLoginRequest.getEmail());
